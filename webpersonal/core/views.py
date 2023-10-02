82e84c89fe4b7dc0d0d3954ff3126d79df9336fb
saulsistemas <santamariaramos18@hotmail.com>
2023-10-02 18:27:00 -0500

    creando templates

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render, HttpResponse
 html_base ="""
-<h1> Mi Web personal </h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/porfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-<ul>
+
 """
 # Create your views here.
 def home(request):
-    htlm = "<h2>Portada</h2>";
-    #bucle for
-    for i in range(10):
-        htlm+="<h5>Prueba</h5>";
-    return HttpResponse(html_base+htlm);
+    #return request;
+    return render(request,"core/home.html");
 
 def about(request):
     nombre = "Saul";
