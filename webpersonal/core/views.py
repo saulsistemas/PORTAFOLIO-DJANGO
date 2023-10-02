a8759dc441b9dd926e26b15d64a735a56eb8b090
saulsistemas <santamariaramos18@hotmail.com>
2023-10-02 18:15:23 -0500

    ruta y comunicacion con el controlador(views)2

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, HttpResponse
-
+html_base ="""
+<h1> Mi Web personal </h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+<ul>
+"""
 # Create your views here.
 def home(request):
-    htlm = "<h1>Es una prueba</h1><h2>Portada</h2>";
+    htlm = "<h2>Portada</h2>";
     #bucle for
     for i in range(10):
-        htlm+="<h3>Prueba</h3>";
-    return HttpResponse(htlm);
+        htlm+="<h5>Prueba</h5>";
+    return HttpResponse(html_base+htlm);
+
+def about(request):
+    nombre = "Saul";
+    return HttpResponse(html_base+"<h2>Acerca de</h2><p>"+nombre+"</p>");
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -21,4 +21,5 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home"),
+    path('about-me/',views.about, name="about"),
 ]
