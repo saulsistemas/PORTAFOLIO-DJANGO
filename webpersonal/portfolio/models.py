8a47d40fdcfafec7d342c69ca764155641f4a1ac
saulsistemas <santamariaramos18@hotmail.com>
2023-10-03 08:13:49 -0500

    Migraciones y crear modelos

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title =  models.CharField;
-    description = models.TextField;
-    image = models.ImageField;
-    created = models.DateTimeField(auto_now_add=True);
-    updated = models.DateTimeField(auto_now=True);
+
+    title =  models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
